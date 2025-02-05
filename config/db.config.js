99ce9add99bdb42994381a00cd0517e2e4ac42b1
Simon K <Simon@DESKTOP-R9UCS0H>
2025-02-05 15:48:12 -0800

    fixes

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/chat_app', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
